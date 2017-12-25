a3f4c3e075772a9471fc43687d3337d373f4c518
Priyank Panchal <prinkpan@gmail.com>
2017-12-26 00:49:43 +0530

    First commit of basic stock calculator

---
+++ BasicStockCalculator/BasicStockCalculator/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using BasicStockCalculator.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BasicStockCalculator
+{
+    public partial class MainPage : ContentPage
+	{
+        public static double EnteredValue { get; set; }
+        public static ObservableCollection<string> Calculations { get; set; }
+
+		public MainPage()
+		{
+            Calculations = new ObservableCollection<string>();
+            InitializeComponent();
+		}
+
+        void OnValueChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                BSCSettingsVM bscSettings = new BSCSettingsVM();
+                Calculations.Clear();
+                if(e.NewTextValue.Length > 0)
+                {
+                    EnteredValue = Convert.ToDouble(e.NewTextValue);
+                    var percent = Convert.ToDouble(bscSettings.StartRange);
+                    for(double i = bscSettings.StartRange; percent < bscSettings.EndRange; i += bscSettings.Step)
+                    {
+                        percent = Math.Round(i, 1);
+                        Calculations.Add(percent + "% : " + String.Format("{0:0.00}", ((i / 100) * EnteredValue) + EnteredValue));
+                    }
+                }
+            }
+            catch
+            {
+                //Handle exception
+            }
+        }
+
+        void OnSettingsClicked(object sender, EventArgs e)
+        {
+            var settings = new SettingsPage();
+            this.Navigation.PushAsync(settings);
+        }
+    }
+}
---
+++ BasicStockCalculator/BasicStockCalculator/ViewModel/BSCSettingsVM.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using BasicStockCalculator.Helpers;
+
+namespace BasicStockCalculator.ViewModel
+{
+    public class BSCSettingsVM : INotifyPropertyChanged
+    {
+        public int StartRange
+        {
+            get => Settings.StartRange;
+            set
+            {
+                if (Settings.StartRange == value)
+                    return;
+                Settings.StartRange = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int EndRange
+        {
+            get => Settings.EndRange;
+            set
+            {
+                if (Settings.EndRange == value)
+                    return;
+                Settings.EndRange = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Step
+        {
+            get => Settings.Step;
+            set
+            {
+                if (Settings.Step == value)
+                    return;
+                Settings.Step = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string name = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        #endregion
+    }
+}
