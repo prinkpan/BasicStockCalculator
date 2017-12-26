8ac1098f4189ea95c6775609abd215226bd97c62
Priyank Panchal <prinkpan@gmail.com>
2017-12-26 19:24:07 +0530

    Implemented separate list for negative and positive values along with color highlight

--- BasicStockCalculator/BasicStockCalculator/MainPage.xaml.cs
+++ BasicStockCalculator/BasicStockCalculator/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using BasicStockCalculator.ViewModel;
+﻿using BasicStockCalculator.Model;
+using BasicStockCalculator.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -10,11 +11,17 @@ namespace BasicStockCalculator
     public partial class MainPage : ContentPage
 	{
         public static double EnteredValue { get; set; }
-        public static ObservableCollection<string> Calculations { get; set; }
+        public static ObservableCollection<Calculation> NegativeCalculations { get; set; }
+        public static ObservableCollection<Calculation> PositiveCalculations { get; set; }
 
-		public MainPage()
+        //Both these shades array starts from dark and goes till light
+        public readonly string[] GreenShades = { "#000E0C", "#001B16", "#002820", "#00352A", "#004234", "#004F3E", "#005C48", "#006952", "#00765C", "#008366", "#1A8F75", "#349B84", "#4EA793", "#68B3A2", "#82BFB1", "#9CCBC0", "#B6D7CF", "#D0E3DE", "#EAEFED", "#ECF1EF", "#EEF3F1", "#F0F5F3", "#F2F7F5", "#F4F9F7", "#F6FBF9" };
+        public readonly string[] RedShades = { "#260908", "#390F0E", "#4C1514", "#5F1B1A", "#722120", "#852726", "#982D2C", "#AB3332", "#BE3938", "#C54D4C", "#CC6160", "#D37574", "#DA8988", "#E19D9C", "#E8B1B0", "#EFC5C4", "#F6D9D8", "#FDEDEC", "#FDEFEE", "#FDF1F0", "#FDF3F2", "#FDF5F4", "#FDF7F6", "#FDF9F8", "#FDFBFA" };
+
+        public MainPage()
 		{
-            Calculations = new ObservableCollection<string>();
+            NegativeCalculations = new ObservableCollection<Calculation>();
+            PositiveCalculations = new ObservableCollection<Calculation>();
             InitializeComponent();
 		}
 
@@ -23,16 +30,48 @@ namespace BasicStockCalculator
             try
             {
                 BSCSettingsVM bscSettings = new BSCSettingsVM();
-                Calculations.Clear();
+                var EndRange = bscSettings.EndRange;
+                var StartRange = bscSettings.StartRange;
+                var Step = bscSettings.Step;
+
+                NegativeCalculations.Clear();
+                PositiveCalculations.Clear();
                 if(e.NewTextValue.Length > 0)
                 {
                     EnteredValue = Convert.ToDouble(e.NewTextValue);
-                    var percent = Convert.ToDouble(bscSettings.StartRange);
-                    for(double i = bscSettings.StartRange; percent < bscSettings.EndRange; i += bscSettings.Step)
+                    var shadeIndex = 0;
+                    for (double i = StartRange; i <= EndRange; i += Step)
                     {
-                        percent = Math.Round(i, 1);
-                        Calculations.Add(percent + "% : " + String.Format("{0:0.00}", ((i / 100) * EnteredValue) + EnteredValue));
+                        if(i <= 0)
+                        {
+                            NegativeCalculations.Add(new Calculation()
+                            {
+                                Data = Math.Round(i, 1) + "% : " + String.Format("{0:0.00}", ((i / 100) * EnteredValue)) + " : " + String.Format("{0:0.00}", ((i / 100) * EnteredValue) + EnteredValue),
+                                BgColor = Color.FromHex("#d26260")
+                            });
+                        }
+                        if(i >= 0)
+                        {
+                            PositiveCalculations.Add(new Calculation()
+                            {
+                                Data = Math.Round(i, 1) + "% : " + String.Format("{0:0.00}", ((i / 100) * EnteredValue)) + " : " + String.Format("{0:0.00}", ((i / 100) * EnteredValue) + EnteredValue),
+                                BgColor = Color.FromHex("#00cca3")
+                            });
+                        }
+                        shadeIndex++;
                     }
+
+                    if(NegativeCalculations.Count == 0)
+                    {
+                        NegativeCalculations.Add(new Calculation() { Data = "Not applicable", BgColor = Color.White });
+                    }
+                    if(PositiveCalculations.Count == 0)
+                    {
+                        PositiveCalculations.Add(new Calculation() { Data = "Not applicable", BgColor = Color.White });
+                    }
+
+                    NegativeListView.ItemsSource = NegativeCalculations;
+                    PositiveListView.ItemsSource = PositiveCalculations;
                 }
             }
             catch
---
+++ BasicStockCalculator/BasicStockCalculator/Model/Calculation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BasicStockCalculator.Model
+{
+    public class Calculation
+    {
+        public string Data { get; set; }
+        public Color BgColor { get; set; }
+    }
+}
